5eee0771367a8b63c35a34e67bced262e0d1d44e
Reddit00 <nifodownikita@gmail.com>
2026-04-13 23:40:14 +0300

    lab30

---
+++ lab30V11/lab30v11.Test/UrlHelperTest.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using lab30v11;
+
+namespace lab30v11.Test;
+
+public class UrlHelperTests
+{
+    private readonly UrlHelper _urlHelper = new UrlHelper();
+
+    // Тести для IsValidUrl 
+    [Theory]
+    [InlineData("https://google.com")]
+    [InlineData("http://microsoft.com/path")]
+    [InlineData("https://sub.domain.edu.ua")]
+    [InlineData("https://chat.openai.com?q=test")]
+    [InlineData("http://127.0.0.1")]
+    public void IsValidUrl_ValidInputs_ReturnsTrue(string url)
+    {
+        Assert.True(_urlHelper.IsValidUrl(url));
+    }
+
+    // Тести для IsValidUrl 
+    [Theory]
+    [InlineData("google.com")]      // без протоколу
+    [InlineData("ftp://files.com")] // непідтримуваний протокол
+    [InlineData("")]               // порожній рядок
+    public void IsValidUrl_InvalidInputs_ReturnsFalse(string url)
+    {
+        Assert.False(_urlHelper.IsValidUrl(url));
+    }
+
+    // Тести для GetDomain
+    [Fact]
+    public void GetDomain_StandardUrl_ReturnsHost()
+    {
+        var result = _urlHelper.GetDomain("https://github.com/explore");
+        Assert.Equal("github.com", result);
+    }
+
+    [Fact]
+    public void GetDomain_WithWww_StripsWww()
+    {
+        var result = _urlHelper.GetDomain("https://www.apple.com");
+        Assert.Equal("apple.com", result);
+    }
+
+    [Fact]
+    public void GetDomain_InvalidUrl_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => _urlHelper.GetDomain("not-a-valid-url"));
+    }
+}
---
+++ lab30V11/lab30v11/UrlHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace lab30v11;
+
+public class UrlHelper
+{
+    public bool IsValidUrl(string? url)
+    {
+        if (string.IsNullOrWhiteSpace(url)) return false;
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+               && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+    }
+
+   public string GetDomain(string url)
+    {
+        if (!IsValidUrl(url))
+            throw new ArgumentException("Invalid URL format");
+
+        var uri = new Uri(url);
+        var host = uri.Host;
+
+        return host.StartsWith("www.", StringComparison.OrdinalIgnoreCase) 
+               ? host[4..] 
+               : host;
+    }
+}
